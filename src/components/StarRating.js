34d2513e66dd3642541a813d590275d8c1a089cb
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-18 10:15:48 -0500

    Added handleClicks function to allow decreasing rating by clicking the same star

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -7,16 +7,24 @@ function StarRating(props) {
   const [rating, setRating] = useState(0);
   const starsArray = [0, 1, 2, 3, 4];
 
+  const handleClicks = (index) => {
+    const clicked = index + 1;
+
+    if (clicked === rating) {
+      setRating(rating - 1);
+    } else {
+      setRating(clicked);
+    }
+    props.openDialog();
+  }
+
   return (
 
     <div className="rating-container">
       <h1>Please, rate our service</h1>
       <ul className="rating-stars">
         {starsArray.map((value, index) => (
-          <Star key={index} StarIcon={props.StarIcon} rated={index < rating} onClick={() => {
-            setRating(index + 1);
-            props.openDialog();
-          }} />
+          <Star key={index} StarIcon={props.StarIcon} rated={index < rating} onClick={() => handleClicks(index)} />
         ))}
       </ul>
       <h2 className="rating-words">{rating > 0 && props.ratingWords[rating - 1]}</h2>
